5cb4d58dc4c42eeb241b1bac9e63769bf362bb19
Hector Gomez <hgl502@hotmail.com>
2023-10-19 01:21:07 -0500

    07-restserver-google

--- 07-restserver/helpers/db-validators.js
+++ 07-restserver/helpers/db-validators.js
@@ -1,5 +1,6 @@
 const Role = require("../models/role");
 const Usuario = require("../models/usuario");
+
 const esRoleValido = async (rol = '') => {
     const existeRol = await Role.findOne({rol});
     if (!existeRol) throw new Error(`El rol ${rol} no esta registrado en la BD`);
--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -6,6 +6,7 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT || 3000;
+        this.authPath = '/api/auth';
         this.usuariosPath = '/api/usuarios';
 
         this.conectarDB();
@@ -24,6 +25,7 @@ class Server {
     }
 
     routes() {
+        this.app.use(this.authPath, require('../routes/auth'));
         this.app.use(this.usuariosPath, require('../routes/usuarios'));
     }
 
