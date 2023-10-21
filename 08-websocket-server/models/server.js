8ddec73ba6d9d6019abf9151958f8900abf6abd3
Hector Gomez <hgl502@hotmail.com>
2023-10-21 17:07:41 -0500

    08-websocket-server

---
+++ 08-websocket-server/models/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require('cors');
+const {socketController} = require("../sockets/controller");
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 3000;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
+        this.paths = {}
+
+        this.middlewares();
+        this.routes();
+        this.sockets();
+    }
+
+    middlewares() {
+        this.app.use(cors());
+        this.app.use(express.static('08-websocket-server/public'));
+    }
+
+    routes() {
+    }
+
+    sockets() {
+        this.io.on('connection', socketController);
+    }
+
+    listen() {
+        this.server.listen(this.port, () => {
+            console.log(`Example app listening on port ${this.port}`);
+        });
+    }
+}
+
+module.exports = Server;
